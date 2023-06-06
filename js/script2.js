034fc539fc87c8daa4a79aefb70d8eb803a95020
Filipe de Bianchi Andrade <fbianchi.andrade@gmail.com>
2023-06-06 10:55:53 -0300

    modificacoes nos scripts e nas classes

--- js/script2.js
+++ js/script2.js
@@ -1,7 +1,3 @@
-var pergunta = prompt(`Você deseja fazer uma lista de compras?
-                Responda: 
-                1) Sim; ou
-                2) Não.`);
 const categorias = [[], [], [], [], []];
 
 const riscaItem = [[], [], [], [], []];
@@ -10,85 +6,69 @@ const idsModificacao = [`frutas`, `laticinios`, `congelados`, `doces`, `outros`]
 
 let qualAlimento;
 
-if (pergunta == '1' || pergunta == 'sim') {
-    perguntaAlimento();
-} else {
-    alert('Volte quando desejar fazer uma lista de compras.')
-}
+marcarProdutos();
 
-function perguntaAlimento() {
-    let qualAlimento = prompt('Qual alimento você deseja adicionar?');
-    let perguntaCategoria = prompt(`Em qual categoria este produto de enquadra? 
-                        1) frutas
-                        2) laticinios
-                        3) congelados
-                        4) doces
-                        5) outros`);
 
-    switch (perguntaCategoria) {
-        case `1`: categorias[0].push(`<li id="${qualAlimento}" class="riscar-item">${qualAlimento}</li>`);
-            riscaItem[0].push(qualAlimento);
-            console.table(categorias);
-            break;
 
-        case `2`: categorias[1].push(`<li id="${qualAlimento}" class="riscar-item">${qualAlimento}</li>`);
-            riscaItem[1].push(qualAlimento);
-            console.table(categorias);
-            break;
+document.querySelector(`.titulo-texto`).addEventListener(`click`, () => {
+    alert(`Para adicionar produtos em sua lista, clique no titulo de cada seção.`);
+});
 
-        case `3`: categorias[2].push(`<li id="${qualAlimento}" class="riscar-item">${qualAlimento}</li>`);
-            riscaItem[2].push(qualAlimento);
-            console.table(categorias);
-            break;
 
-        case `4`: categorias[3].push(`<li id="${qualAlimento}" class="riscar-item">${qualAlimento}</li>`);
-            riscaItem[3].push(qualAlimento);
-            console.table(categorias);
-            break;
+function marcarProdutos() {
 
-        case `5`: categorias[4].push(`<li id="${qualAlimento}" class="riscar-item">${qualAlimento}</li>`);
-            riscaItem[4].push(qualAlimento);
-            console.table(categorias)
-            break;
-    }
+    document.querySelector(`.titulo-frutas`).addEventListener(`click`, () => {
+        let qualAlimento = prompt('Qual produto você deseja adicionar?');
 
-    var pergunta = prompt(`Deseja adicionar mais algum produto?
+        categorias[0].push(`<li id="${qualAlimento}" class="riscar-item">${qualAlimento}</li>`);
+        riscaItem[0].push(qualAlimento);
+        console.table(categorias);
+
+        var pergunta = prompt(`Deseja adicionar mais algum produto?
             Responda 
             1) Sim;
             2) Não.`);
 
-    if (pergunta == '1' || pergunta == 'sim') {
-        perguntaAlimento();
-    } else {
-        alert('Sua lista está pronta.');
+        if (pergunta == '1' || pergunta == 'sim') {
+            marcarProdutos();
+        } else {
+            alert('Sua lista está pronta.');
 
-        for (var i = 0; i < categorias.length; i++) {
-            var conteudo = "";
-            for (var j = 0; j < categorias[i].length; j++) {
-                conteudo += categorias[i][j] + " ";
+            for (var i = 0; i < categorias.length; i++) {
+                var conteudo = "";
+                for (var j = 0; j < categorias[i].length; j++) {
+                    conteudo += categorias[i][j] + " ";
+                }
+                document.getElementById(idsModificacao[i]).innerHTML = conteudo;
             }
-            document.getElementById(idsModificacao[i]).innerHTML = conteudo;
-        }
 
+        }
     }
+    );
 }
+
+
+
+
+
+
 
 for (var i = 0; i < riscaItem.length; i++) {
     for (var j = 0; j < riscaItem[i].length; j++) {
         var item = riscaItem[i][j];
         document.getElementById(item).addEventListener('click', adicionaRiscoVermelho(item));
         document.getElementById(item).addEventListener('dblclick', adicionaRiscoVermelho2(item));
-    }     
+    }
 }
 
 function adicionaRiscoVermelho(item) {
-    return function() {
+    return function () {
         document.getElementById(item).className = 'riscar-item2';
     };
 }
 
 function adicionaRiscoVermelho2(item) {
-    return function() {
+    return function () {
         document.getElementById(item).className = 'riscar-item';
     };
 }

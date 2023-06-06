6db52ed4366c580866c8027f01a92bbde35694d3
Filipe de Bianchi Andrade <fbianchi.andrade@gmail.com>
2023-06-06 09:24:59 -0300

    modificando para o double click retirar o risco vermelho

--- js/script.js
+++ js/script.js
@@ -77,11 +77,18 @@ for (var i = 0; i < riscaItem.length; i++) {
     for (var j = 0; j < riscaItem[i].length; j++) {
         var item = riscaItem[i][j];
         document.getElementById(item).addEventListener('click', createClickHandler(item));
+        document.getElementById(item).addEventListener('dblclick', createClickHandler2(item));
     }     
 }
 
 function createClickHandler(item) {
     return function() {
         document.getElementById(item).className = 'riscar-item2';
+    };
+}
+
+function createClickHandler2(item) {
+    return function() {
+        document.getElementById(item).className = 'riscar-item';
     };
 }
